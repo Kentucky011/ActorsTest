7126fb952188bfad59c6046e50469c126a034ef8
Kentucky011 <Admin@DESKTOP-5IRF6KL.(none)>
2024-04-17 11:29:32 +0500

    add pingPong

--- src/main/scala/ActorTest.scala
+++ src/main/scala/ActorTest.scala
@@ -1,3 +1,47 @@
-object ActorTest {
+import akka.actor._
 
+object ActorTest extends App {
+
+  val system = ActorSystem("PingPongSystem")
+  val actorPong = system.actorOf(Props[ActorPong], "ActorPong")
+  val actorPing = system.actorOf(Props(new ActorPing(actorPong)), "ActorPing")
+  actorPing ! "start"
+  case object Pong
+  case object Ping
+  system.terminate()
+  class ActorPing(actorPong: ActorRef) extends Actor {
+    var counter = 5
+
+    def receive = {
+      case "start" =>
+        println("Актор ПИНГ: Отправляю сообщение ПИНГ актору ПОНГ")
+        actorPong ! Ping
+      case Pong =>
+        counter -= 1
+        if (counter > 0) {
+          println("Актор ПИНГ: получил сообщение ПОНГ от актора ПОНГ, отправляю ПИНГ")
+          actorPong ! Ping
+        } else {
+          println("Актор ПИНГ: Лимит сообщений исчерпан, завершение.")
+          context.stop(self)
+        }
+    }
+
+    override def preStart(): Unit = {
+      println(s"Актор ПИНГ: количество обрабатываемых сообщение равно $counter")
+    }
+  }
+
+  class ActorPong extends Actor {
+
+    def receive = {
+      case Ping =>
+        println("Актор ПОНГ: получил сообщение ПИНГ от актора ПИНГ, отправляю ПОНГ")
+        sender() ! Pong
+      case _ =>
+        println("Актор ПОНГ: получил сообщение ПИНГ от актора ПИНГ, отправляю ПОНГ")
+        sender() ! Pong
+    }
+
+  }
 }
